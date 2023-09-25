5ece810d9c318bd79dd384b9336c320bc23f2bd9
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2023-09-25 18:11:09 +0500

    f7

--- build_func07.py
+++ build_func07.py
@@ -1,4 +1,5 @@
-def main(x, y):
+from math import *
+def main(x,y):
     """Integer type variables 'x' and 'y' are given. Return the value of the expression in README.md file.
     https://github.com/codeschool43/Build_in_function_homework#build_func07
 
@@ -9,4 +10,7 @@ def main(x, y):
     Returns:
         int: the value of the expression
     """
-    return
+    return int(pow(x,2)+6*pow(x,3)+3*x*y)
+x=int(input())
+y=int(input())
+print(main(x,y))
