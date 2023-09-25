55ed86cd74d3fc4eb2d370a73cb8c82c9e612aaf
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2023-09-25 18:13:17 +0500

    f10

--- build_func10.py
+++ build_func10.py
@@ -1,3 +1,4 @@
+from math import *
 def main(x, y):
     """Integer type variables 'x' and 'y' are given. Return the value of the expression in README.md file.
     https://github.com/codeschool43/Build_in_function_homework#build_func10
@@ -9,4 +10,7 @@ def main(x, y):
     Returns:
         int: the value of the expression
     """
-    return
+    return round((3*pow(y,1/2)+pow(x,2/3)),2)
+x=int(input())
+y=int(input())
+print(main(x,y))
